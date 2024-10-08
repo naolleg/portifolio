bb49fc62da0e7b816b6f1589bd95c2504c46c528
naolleg <naollegesse51@gmail.com>
2024-10-08 22:56:39 +0300

    some change

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http')
 const app = express()
 app.use(express.static('build'))
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 const server = http.createServer(app)
 
